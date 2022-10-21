f178726c95bd39f7e29c2293e7ace3390696e9f3
VTrofym <v.trof@icloud.com>
2022-10-21 09:42:17 +0300

    start marcup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { Component } from 'react';
+// import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+  
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <div>
+          <h4>Name</h4>
+        <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+        />
+        <div>
+          <button type='button'>Add contact</button>
+          </div>
+        </div>
+          <h2>Contacts</h2>
+          <ul>
+            <li>Rosie Simpson</li>
+          </ul>
     </div>
   );
+  }
+  
 };
